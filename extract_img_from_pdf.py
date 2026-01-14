4fa59b7ff8570d9e03632b16d19d3d456575ac88
Raiyan <69578720+FarooquiRaiyan@users.noreply.github.com>
2026-01-14 17:47:44 +0530

    Add files via upload

---
+++ extract_img_from_pdf.py
@@ -0,0 +1,28 @@
+import fitz  # PyMuPDF
+import os
+
+pdf_path = "C:/Users/Raiyan/Downloads/VEGA Cold Chain Compliance Stack-1-20.pdf"
+output_dir =  "extracted_images"
+
+os.makedirs(output_dir, exist_ok=True)
+
+doc = fitz.open(pdf_path)
+img_count = 0
+
+for page_index in range(len(doc)):
+    page = doc[page_index]
+    images = page.get_images(full=True)
+    
+    for img_index, img in enumerate(images):
+        xref = img[0]
+        base_image = doc.extract_image(xref)
+        image_bytes = base_image["image"]
+        image_ext = base_image["ext"]
+
+        img_count += 1
+        image_filename = f"image_{img_count}.{image_ext}"
+
+        with open(os.path.join(output_dir, image_filename), "wb") as f:
+            f.write(image_bytes)
+            
+print(f"✅ Extracted {img_count} images successfully!")
